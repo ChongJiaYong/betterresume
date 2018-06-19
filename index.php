7dd413e95d73003c6a89c22aa0355dbbd98b4115
Jia Yong Chong <williamcjjy@live.com>
2018-06-19 09:40:08 +0000

    Update to layout of website and added images

--- index.php
+++ index.php
@@ -42,31 +42,31 @@
 	<h1 align="center">Education</h1>
 		<section class="experience">
 			<div class="row">
-				<div class="grid-2">
-					
-					<h2><strong><a style="color:grey; text-decoration:none" href="http://www.anu.edu.au/" target = "blank">Australia National University</a></strong></h2>
-					<p>Jan 2018 - Expected Dec 2019</p>
+				<div class="grid-1">
+					<a  href="http://www.anu.edu.au/" target = "blank"><div class="anu-logo logo"></div></a>
 				</div>
-				<div class="grid-4">
+				<div class="grid-5">
+					<h2 class="credential-heading">Australia National University</h2>
+					<p>Jan 2018 - Expected Dec 2019</p>
 					<ul>
 						<li>Received the 2018 ANU College of Business and Economics International Undergraduate Award </li></br>
 					</ul>
 				</div>
-			</div>
+			</div></br>
 
 				<div class="row">
-				<div class="grid-2">
-					
-					<h2><strong><a style="color:grey; text-decoration:none" href="http://www.help.edu.my/" target = "blank">HELP University</a></strong></h2>
-					<p>Jan 2017 - Dec 2017</p>
+					<div class="grid-1">
+						<a href="http://www.help.edu.my/" target = "blank"><div class="help-logo logo"></div></a>
+					</div>
+					<div class="grid-5">
+						<h2 class="credential-heading">HELP University</h2>
+						<p>Jan 2017 - Dec 2017</p>
+						<ul>
+							<li>Received a 70% merit scholarship </li></br>
+							<li>Achieved a CGPA of 3.83/4.0 as of Year 1, Sem 3</li>
+						</ul>
+					</div>	
 				</div>
-				<div class="grid-4">
-					<ul>
-						<li>Received a 70% merit scholarship </li></br>
-						<li>Achieved a CGPA of 3.83/4.0 as of Year 1, Sem 3</li>
-					</ul>
-				</div>
-			</div>
 			
 			<hr>
 			
@@ -74,12 +74,13 @@
 		<h1 align="center">Professional experience</h1>
 		<section class="experience">
 			<div class="row">
-				<div class="grid-2">
-					<h2><strong><a style="color:grey; text-decoration:none" href="http://myburgerlab.com" target = "blank">myBurgerLab</a></strong></h2>
-					<h4><em>Kitchen staff and Waiter</em></h4>
-					<p>Sep. 2017 - Jan. 2018</p>
+				<div class="grid-1">
+					<a  href="https://myburgerlab.com" target = "blank"><div class="myburgerlab-logo logo"></div></a>
 				</div>
-				<div class="grid-4">
+				<div class="grid-5">
+					<h2><strong><a style="color:grey; text-decoration:none" >myBurgerLab</a></strong></h2></br>
+					<em>Kitchen staff and Waiter</em>
+					<p>Sep. 2017 - Jan. 2018</p>
 					<ul>
 						<li>Have accurately processed up to 500 orders per night while communicating effectively with my co-workers, thus creating a smooth work flow within the kitchen and maximizing sales of up to RM8,000 per day. </li></br>
 						<li>Received achievement awards from management in the form of cards for being proficient on the griddle and the assembly line. </li></br>
@@ -89,12 +90,13 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="grid-2">
-					<h2><a style="color:grey; text-decoration:none" href="https://www.popular.com.sg/" target = "blank">POPULAR</a></h2>
-					<h4><em>Retail Assistant</em></h4>
-					<p>Aug. 2017</p>
+				<div class="grid-1">
+					<a href="https://www.popular.com.sg/" target = "blank"><div class="popular-logo logo"></div></a>
 				</div>
-				<div class="grid-4">
+				<div class="grid-5">
+					<h2><a style="color:grey; text-decoration:none" href="https://www.popular.com.sg/" target = "blank">POPULAR</a></br></h2>
+					<em>Retail Assistant</em>
+					<p>Aug. 2017</p>
 					<ul>
 						<li>Communicated effectively with customers to uphold customer satisfaction levels.</li></br>
 						<li>Demonstrated the ability to pay attention to details by identifying and assisting customers whom require assistance without them requesting for help first.</li></br>
@@ -109,11 +111,12 @@
 		<h1 align="center">Voluntary work and Extra co-curricular activities</h1>
 		<section class="experience">
 			<div class="row">
-				<div class="grid-2">
-					<h2><strong><a style="color:grey; text-decoration:none" href="http://vocalact.webs.com/" target = "blank">Victims of Crime Assistance League</a></strong></h2>
-					<p>May 2018 – Current</p>
+				<div class="grid-1">
+					<a href="http://vocalact.webs.com/" target = "blank"><div class="vocal-logo logo"></div></a>
 				</div>
-				<div class="grid-4">
+				<div class="grid-5">
+					<h2><strong>Victims of Crime Assistance League</a></strong></h2>
+					<p>May 2018 – Current</p>
 					<ul>
 						<li>Acting as the administrative assistant for VOCAL.</li></br>
 						<li>Currently working on the preparation of VOCAL’s financial statement and a budget to project the cash flow required by VOCAL. </li></br>
@@ -122,11 +125,12 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="grid-2">
-					<h2><strong><a style="color:grey; text-decoration:none" href="https://www.facebook.com/MSO.at.ANU/" target = "blank">MSO ANU</a></strong></h2>
-					<p>Mar. 2018 – Current</p>
+				<div class="grid-1">
+					<a href="https://www.facebook.com/MSO.at.ANU/" target = "blank"><div class="mso-logo logo"></div></a>
 				</div>
-				<div class="grid-4">
+				<div class="grid-5">
+					<h2><strong>MSO ANU</a></strong></h2>
+					<p>Mar. 2018 – Current</p>
 					<ul>
 						<li>Acted as the head of resource for a food competition held for 27 contestants and an audience of about 70 people.</li></br>
 						<li>Developed critical thinking skills in determining the amount of resources that needs to be purchased and to ensure the resources purchased stayed within the allocated budget. </li></br>
@@ -135,11 +139,12 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="grid-2">
-					<h2><strong><a style="color:grey; text-decoration:none" href="https://www.facebook.com/helpcos/" target = "blank">Community Outreach Society</a></strong></h2>
-					<p>Jan. 2017 – Dec. 2017</p>
+				<div class="grid-1">
+					<a href="https://www.facebook.com/helpcos/" target = "blank"><div class="cos-logo logo"></div></a>
 				</div>
-				<div class="grid-4">
+				<div class="grid-5">
+					<h2><strong>Community Outreach Society</a></strong></h2>
+					<p>Jan. 2017 – Dec. 2017</p>
 					<ul>
 						<li>Acted as the treasurer and head of logistics for a 3 days 2 nights camp participated by 50 volunteers to an urban area held for roughly 150 students.</li></br>
 						<li>Acted as the treasurer and secretary for a half day science camp for 60 11-year-old students.</li></br>
@@ -149,11 +154,12 @@
 				</div>
 			</div>
 			<div class="row">
-				<div class="grid-2">
-					<h2><a style="color:grey; text-decoration:none" href="https://goo.gl/G8Yybf" target = "blank">Bizball Committee</a></h2>
-					<p>Jan. 2017 – Oct. 2017</p>
+				<div class="grid-1">
+					<a href="https://goo.gl/G8Yybf" target = "blank"><div class="bizball-logo logo"></div></a>
 				</div>
-				<div class="grid-4">
+				<h2>Bizball Committee</a></h2>
+					<p>Jan. 2017 – Oct. 2017</p>
+				<div class="grid-5">
 					<ul>
 						<li>Assisted in organising a bazaar-style event which raised about RM2500 for the event. </li></br>
 						<li>Handled transportation to and fro from the venue for 61 participants during the event.</li>
